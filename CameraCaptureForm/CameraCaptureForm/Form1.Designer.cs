f528e581883d7f03422603671528b685b00ddab6
Tristan <tristan-soanes@hotmail.co.uk>
2017-12-28 17:58:38 +0000

    GUI - Initial Commit with GUI
    Has starter GUI components, a basic camera feed and face detection, no recognition.

---
+++ CameraCaptureForm/CameraCaptureForm/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CameraCaptureForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_StartCapture = new System.Windows.Forms.Button();
+            this.btn_StopCapture = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(61, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1012, 466);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_StartCapture
+            // 
+            this.btn_StartCapture.Location = new System.Drawing.Point(61, 12);
+            this.btn_StartCapture.Name = "btn_StartCapture";
+            this.btn_StartCapture.Size = new System.Drawing.Size(119, 35);
+            this.btn_StartCapture.TabIndex = 1;
+            this.btn_StartCapture.Text = "Start Capture";
+            this.btn_StartCapture.UseVisualStyleBackColor = true;
+            this.btn_StartCapture.Click += new System.EventHandler(this.btn_StartCapture_Click);
+            // 
+            // btn_StopCapture
+            // 
+            this.btn_StopCapture.Location = new System.Drawing.Point(214, 12);
+            this.btn_StopCapture.Name = "btn_StopCapture";
+            this.btn_StopCapture.Size = new System.Drawing.Size(133, 35);
+            this.btn_StopCapture.TabIndex = 2;
+            this.btn_StopCapture.Text = "Stop Capture";
+            this.btn_StopCapture.UseVisualStyleBackColor = true;
+            this.btn_StopCapture.Click += new System.EventHandler(this.btn_StopCapture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 578);
+            this.Controls.Add(this.btn_StopCapture);
+            this.Controls.Add(this.btn_StartCapture);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_StartCapture;
+        private System.Windows.Forms.Button btn_StopCapture;
+    }
+}
+
---
+++ CameraCaptureForm/CameraCaptureForm/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace CameraCaptureForm
+{
+    public partial class Form1 : Form
+    {
+        // Declaring constants
+        static string haarFaceFile = @"C:\Emgu\emgucv-windesktop 3.3.0.2824\opencv\data\haarcascades\haarcascade_frontalface_default.xml";
+        static string haarEyeFile = @"C:\Emgu\emgucv-windesktop 3.3.0.2824\opencv\data\haarcascades\haarcascade_eye.xml";
+
+        VideoCapture cameraCapture;
+
+        // Creating classifiers here
+        CascadeClassifier faceClassifier = new CascadeClassifier(haarFaceFile);
+        CascadeClassifier eyeClassifier = new CascadeClassifier(haarEyeFile);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_StartCapture_Click(object sender, EventArgs e)
+        {
+            if(cameraCapture == null)
+            {
+                cameraCapture = new VideoCapture();
+            }
+
+            cameraCapture.ImageGrabbed += CameraCapture_ImageGrabbed;
+            cameraCapture.Start();
+        }
+
+        private void CameraCapture_ImageGrabbed(object sender, EventArgs e)
+        {
+            // Getting the image from the camera
+            Mat capturedImage = new Mat();
+            cameraCapture.Retrieve(capturedImage);
+            var convertedCapture = capturedImage.ToImage<Bgr, byte>();
+
+            // Face detection
+            var greyImage = convertedCapture.Convert<Gray, byte>();
+            var allFaces = faceClassifier.DetectMultiScale(greyImage, 1.1, 10);
+            var allEyes = eyeClassifier.DetectMultiScale(greyImage, 1.1, 10);
+
+            foreach(var face in allFaces)
+            {
+                convertedCapture.Draw(face, new Bgr(Color.Green), 2);
+            }
+
+            foreach (var eye in allEyes)
+            {
+                convertedCapture.Draw(eye, new Bgr(Color.Purple), 2);
+            }
+
+            pictureBox1.Image = convertedCapture.Bitmap;
+        }
+
+        private void btn_StopCapture_Click(object sender, EventArgs e)
+        {
+            if(cameraCapture != null)
+            {
+                cameraCapture = null;
+            }
+        }
+    }
+}
