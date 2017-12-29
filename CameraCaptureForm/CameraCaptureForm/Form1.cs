5fd65c13e0ce4f3b9eb6f78aa2e6dced0a98ce58
Tristan <tristan-soanes@hotmail.co.uk>
2017-12-29 20:17:33 +0000

    Using Resource Files
    Attempting to use the resource files added by previous commit

--- CameraCaptureForm/CameraCaptureForm/Form1.cs
+++ CameraCaptureForm/CameraCaptureForm/Form1.cs
@@ -16,8 +16,8 @@ namespace CameraCaptureForm
     public partial class Form1 : Form
     {
         // Declaring constants
-        static string haarFaceFile = @"C:\Emgu\emgucv-windesktop 3.3.0.2824\opencv\data\haarcascades\haarcascade_frontalface_default.xml";
-        static string haarEyeFile = @"C:\Emgu\emgucv-windesktop 3.3.0.2824\opencv\data\haarcascades\haarcascade_eye.xml";
+        static string haarFaceFile = @"C:\Users\User\Documents\GitHub\Facial_Authentication_Project\CameraCaptureForm\CameraCaptureForm\Resources\haarcascade_frontalface_default.xml";
+        static string haarEyeFile = @"C:\Users\User\Documents\GitHub\Facial_Authentication_Project\CameraCaptureForm\CameraCaptureForm\Resources\haarcascade_eye.xml";
 
         VideoCapture cameraCapture;
 
--- CameraCaptureForm/CameraCaptureForm/Properties/Resources.Designer.cs
+++ CameraCaptureForm/CameraCaptureForm/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CameraCaptureForm.Properties
-{
-
-
+namespace CameraCaptureForm.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,90 @@ namespace CameraCaptureForm.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CameraCaptureForm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot;?&gt;
+        ///&lt;!--
+        ///    Stump-based 20x20 frontal eye detector.
+        ///    Created by Shameem Hameed (http://umich.edu/~shameem)
+        ///
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///
+        ///  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+        ///
+        ///  By downloading, copying, installing or using the software you agree to this license.
+        ///  If you do not agree to this license, do not download, install,
+        ///  copy or use the software.
+        ///
+        ///
+        ///                        Intel License Agr [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string haarcascade_eye {
+            get {
+                return ResourceManager.GetString("haarcascade_eye", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot;?&gt;
+        ///&lt;!--
+        ///    Stump-based 24x24 discrete(?) adaboost frontal face detector.
+        ///    Created by Rainer Lienhart.
+        ///
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///
+        ///  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+        ///
+        ///  By downloading, copying, installing or using the software you agree to this license.
+        ///  If you do not agree to this license, do not download, install,
+        ///  copy or use the software.
+        ///
+        ///
+        ///                        Intel License Agreeme [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string haarcascade_frontalface_default {
+            get {
+                return ResourceManager.GetString("haarcascade_frontalface_default", resourceCulture);
             }
         }
     }
