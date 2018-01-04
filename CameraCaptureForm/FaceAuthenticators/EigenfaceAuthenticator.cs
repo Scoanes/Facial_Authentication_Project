ab4d568598789ff2f278d95d0c6c5d854f78808c
Tristan <tristan-soanes@hotmail.co.uk>
2018-01-04 18:44:33 +0000

    Auth - added project for face authenicators
    Added the project for facial authenticators, set the correct project build settings and added all of the Emgu files needed

---
+++ CameraCaptureForm/FaceAuthenticators/EigenfaceAuthenticator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceAuthenticators
+{
+    public class EigenfaceAuthenticator
+    {
+
+        // Creates the eigenfaces
+        public void TrainEigenfaceAutrhenticator()
+        {
+            // get the faces to train on
+
+            // unfold them
+
+            // create a matrix of each face vector
+
+            // PCA on the matrix
+
+            // Refold to create the Eigenfaces
+        }
+    }
+}
