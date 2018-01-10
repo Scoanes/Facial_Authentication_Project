32eea4367d5b9486274c8ef9fc80d472022c23b4
Tristan <tristan-soanes@hotmail.co.uk>
2018-01-10 15:30:43 +0000

    Testing - Added testrunner project
    Addded the project that will be in charge of testing the recognisers

---
+++ CameraCaptureForm/TestRunner/TestRunner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu.CV;
+using Emgu.CV.Face;
+
+namespace TestRunner
+{
+    public class TestRunner
+    {
+        static void Main()
+        {
+            // call the relevant testing from here
+            //var model = 
+            //FaceRecognizer eigenfaceRecognizer = new EigenFaceRecognizer();
+
+        }
+
+        private void getAllTrainingImages(string rootFolderLocation)
+        {
+
+        }
+    }
+}
