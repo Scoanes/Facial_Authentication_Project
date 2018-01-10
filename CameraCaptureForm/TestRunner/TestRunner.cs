61213ac72968cc4c91cd21f868306f8547226e72
Tristan <tristan-soanes@hotmail.co.uk>
2018-01-10 21:47:18 +0000

    Test - added images grab
    Now gets images and splits them randomly between training and testing sets, then calls the predictor to test each test image

--- CameraCaptureForm/TestRunner/TestRunner.cs
+++ CameraCaptureForm/TestRunner/TestRunner.cs
@@ -14,52 +14,82 @@ namespace TestRunner
 {
     public class TestRunner
     {
+        private static string imageLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestImages");
+        private static int numOfTestElements = 2;
+        private static int numOfElementsPerPerson = 15;
+        private static int numOfTrainImagesPerPerson = numOfElementsPerPerson - numOfTestElements;
+        private static int numOfDifferentTestPeople = Directory.GetDirectories(imageLocation).Length;
+
         static void Main()
         {
-            // call the relevant testing from here
+            // create our randomizer
+            Random randomizer = new Random();
+            int[] elementsToRemove = new int[numOfTestElements];
+
+            // create an array of index's to be removed
+            for (int i = 0; i < numOfTestElements; i++)
+            {
+                elementsToRemove[i] = randomizer.Next(0, numOfElementsPerPerson);
+            }
 
             // create the recogniser objects
             FaceRecognizer eigenfaceRecognizer = new EigenFaceRecognizer();
 
-            // Get Images from test location
-            var imageLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestImages");
-            var testImages = GetAllTrainingImages(imageLocation);
+            // get the training and test data
+            var trainingImages = new List<Mat>();
+            var trainingLabels = new List<int>();
+            var testImages = new List<Mat>();
+            var testLabels = new List<int>();
 
-            // Get the labels for each image
-            var testLabels = GetLabelsForTrainingImages(imageLocation);
+            GetAllTrainingAndTestData(imageLocation, elementsToRemove, ref trainingImages, ref trainingLabels, ref testImages, ref testLabels);
 
             // need to convert both of these into a datatype the Train method accepts
-            var vectorOfImages = new VectorOfMat(testImages.ToArray());
-            var vectorOfLabels = new VectorOfInt(testLabels.ToArray());
+            var vectorOfTrainingImages = new VectorOfMat(trainingImages.ToArray());
+            var vectorOfTrainingLabels = new VectorOfInt(trainingLabels.ToArray());
 
             // Train the recogniser on the images
-            eigenfaceRecognizer.Train(new VectorOfMat(testImages.ToArray()), vectorOfLabels);
+            eigenfaceRecognizer.Train(vectorOfTrainingImages, vectorOfTrainingLabels);
 
+            // need to convert both of these into a datatype the Train method accepts
+            var vectorOfTestImages = new VectorOfMat(testImages.ToArray());
+            var vectorOfTestLabels = new VectorOfInt(testLabels.ToArray());
+
+            // Test the Recognizer
+            for(int i = 0; i < vectorOfTestImages.Size; i++)
+            {
+                eigenfaceRecognizer.Predict(vectorOfTestImages[i]);
+            }
         }
 
-        private static List<Mat> GetAllTrainingImages(string rootFolderLocation)
+        private static void GetAllTrainingAndTestData(string rootFolderLocation, int[] testIndexValues, ref List<Mat> trainImages, ref List<int> trainLabels, ref List<Mat> testImages, ref List<int> testLabels)
         {
-            // create our list of image files
-            List<Mat> allTestImages = new List<Mat>();
-
-            foreach(var imageFile in Directory.GetFiles(rootFolderLocation, "*", SearchOption.AllDirectories))
+            // go through top level directory for each test person
+            foreach(var directory in Directory.GetDirectories(rootFolderLocation))
             {
-                allTestImages.Add(new Mat(imageFile));
+                int iter = 0;
+                foreach (var imageFile in Directory.GetFiles(directory))
+                {
+                    if (iter == testIndexValues[0])
+                    {
+                        testImages.Add(new Image<Gray, byte>(imageFile).Mat);
+                        testLabels.Add(Convert.ToInt32(Path.GetFileName(Path.GetDirectoryName(imageFile))));
+                        //testIndexValues[0] += numOfTrainImagesPerPerson;
+                    }
+                    else if (iter == testIndexValues[1])
+                    {
+                        testImages.Add(new Image<Gray, byte>(imageFile).Mat);
+                        testLabels.Add(Convert.ToInt32(Path.GetFileName(Path.GetDirectoryName(imageFile))));
+                        //testIndexValues[1] += numOfTrainImagesPerPerson;
+                    }
+                    else
+                    {
+                        trainImages.Add(new Image<Gray, byte>(imageFile).Mat);
+                        trainLabels.Add(Convert.ToInt32(Path.GetFileName(Path.GetDirectoryName(imageFile))));
+                    }
+
+                    iter++;
+                }
             }
-
-            return allTestImages;
-        }
-
-        private static List<int> GetLabelsForTrainingImages(string rootFolderLocation)
-        {
-            List<int> trainingImageLabels = new List<int>();
-
-            foreach (var imageFile in Directory.GetFiles(rootFolderLocation, "*", SearchOption.AllDirectories))
-            {
-                trainingImageLabels.Add(Convert.ToInt32(Path.GetFileName(Path.GetDirectoryName(imageFile))));
-            }
-
-            return trainingImageLabels;
         }
     }
 }
