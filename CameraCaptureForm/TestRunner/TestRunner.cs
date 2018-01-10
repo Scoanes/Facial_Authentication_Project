b40889a102377a5d0e96258d13cb86f583fd0f3e
Tristan <tristan-soanes@hotmail.co.uk>
2018-01-10 16:37:58 +0000

    Test - Added images and recognizer
    Added a sample of the images to be trained on, also setup the built in recognizer for eigenFaces

--- CameraCaptureForm/TestRunner/TestRunner.cs
+++ CameraCaptureForm/TestRunner/TestRunner.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 using Emgu.CV;
 using Emgu.CV.Face;
+using System.IO;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
 
 namespace TestRunner
 {
@@ -14,14 +17,49 @@ namespace TestRunner
         static void Main()
         {
             // call the relevant testing from here
-            //var model = 
-            //FaceRecognizer eigenfaceRecognizer = new EigenFaceRecognizer();
+
+            // create the recogniser objects
+            FaceRecognizer eigenfaceRecognizer = new EigenFaceRecognizer();
+
+            // Get Images from test location
+            var imageLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestImages");
+            var testImages = GetAllTrainingImages(imageLocation);
+
+            // Get the labels for each image
+            var testLabels = GetLabelsForTrainingImages(imageLocation);
+
+            // need to convert both of these into a datatype the Train method accepts
+            var vectorOfImages = new VectorOfMat(testImages.ToArray());
+            var vectorOfLabels = new VectorOfInt(testLabels.ToArray());
+
+            // Train the recogniser on the images
+            eigenfaceRecognizer.Train(new VectorOfMat(testImages.ToArray()), vectorOfLabels);
 
         }
 
-        private void getAllTrainingImages(string rootFolderLocation)
+        private static List<Mat> GetAllTrainingImages(string rootFolderLocation)
         {
+            // create our list of image files
+            List<Mat> allTestImages = new List<Mat>();
 
+            foreach(var imageFile in Directory.GetFiles(rootFolderLocation, "*", SearchOption.AllDirectories))
+            {
+                allTestImages.Add(new Mat(imageFile));
+            }
+
+            return allTestImages;
+        }
+
+        private static List<int> GetLabelsForTrainingImages(string rootFolderLocation)
+        {
+            List<int> trainingImageLabels = new List<int>();
+
+            foreach (var imageFile in Directory.GetFiles(rootFolderLocation, "*", SearchOption.AllDirectories))
+            {
+                trainingImageLabels.Add(Convert.ToInt32(Path.GetFileName(Path.GetDirectoryName(imageFile))));
+            }
+
+            return trainingImageLabels;
         }
     }
 }
