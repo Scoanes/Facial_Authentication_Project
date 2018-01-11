84cdcc139b7d3e9174a7ba5d2071686d91f3a822
Tristan <tristan-soanes@hotmail.co.uk>
2018-01-11 18:21:27 +0000

    Solution - Corrected naming
    Updated the solution with correct names for projects and alike

--- CameraCaptureForm/CameraCaptureForm/MainWindow.cs
+++ CameraCaptureForm/CameraCaptureForm/MainWindow.cs
@@ -14,12 +14,12 @@ using System.IO;
 
 namespace CameraCaptureForm
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
 
         bool isMainPage;
 
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
 
